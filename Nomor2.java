0e42db81455f5f98b692a4c873d3fc5c127e5f87
Vemas seftaesa dwi setiawan <vmasstwn@gmail.com>
2024-10-24 08:44:25 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,18 @@
 public class Nomor2{
     public static void main(String[] args){
+  Scanner scanner = new Scanner;
+        
+        int biaya perkilo meter 13000;      
 
+        System.out.println(Masukkan nama driver :);
+        System.out.println(Lama trip : );
+        System.out.println(Total trip);
+        System.out.println(omzet tertinggi yang diperoleh: );
+
+        if (condition) {
+            
+        }
     }
 }
+
+    
