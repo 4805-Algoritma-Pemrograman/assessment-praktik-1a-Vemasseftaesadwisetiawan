d028e2c2d04369e8a3774add597e2dfe41d928f2
Vemas seftaesa dwi setiawan <vmasstwn@gmail.com>
2024-10-24 08:42:46 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,18 @@
-public class Nomor1{
-    public static void main(String[] args){
+public class Nomor1 {
+    public static void main(String[] args) {
+        Scanner scanner= new scanner;
+         var= jam lembur,Honor lembur;
+        int= jam lembur=10000,
+     
+    
 
+    System.out.println(Total jam lembur);
+    System.out.println(Honor jam lembur);
+
+    if (jam lembur > 16*;) {
+        else(jam lembur < 16 *Honor)
+        
+    
+        
     }
 }
